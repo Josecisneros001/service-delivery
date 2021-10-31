f2454ce0a1bc1b51da8cd983e4fe39bfd27d40fe
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2021-10-31 01:00:52 -0500

    iadded users signup header

---
+++ frontend/src/components/UserSignUp/UserSignUp.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './UserSignUp.css';
+
+class UserSignUp extends Component {
+    render() {
+        return (
+            <div className='header'>
+            
+                Welcome to Service Delivery!
+                
+            </div>
+        );
+    }
+    
+}
+
+export default UserSignUp;
