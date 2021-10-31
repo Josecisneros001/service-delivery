7b8eaeb8c7cb9bba67a9d69c040f89b162b33e5f
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2021-10-31 01:17:18 -0500

    added blue square

--- frontend/src/components/UserSignUp/UserSignUp.tsx
+++ frontend/src/components/UserSignUp/UserSignUp.tsx
@@ -4,10 +4,12 @@ import './UserSignUp.css';
 class UserSignUp extends Component {
     render() {
         return (
+            <div className='square'>
             <div className='header'>
             
                 Welcome to Service Delivery!
                 
+            </div>
             </div>
         );
     }
