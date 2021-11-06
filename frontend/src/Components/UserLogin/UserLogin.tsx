66cd60a8c5adfc4ed25f9e30b121b7faa2e830b3
Alex Vargas <alexjesusvargas@gmail.com>
2021-11-06 15:10:58 -0600

    added responsive line

--- frontend/src/Components/UserLogin/UserLogin.tsx
+++ frontend/src/Components/UserLogin/UserLogin.tsx
@@ -16,21 +16,20 @@ class UserLogin extends Component {
         </div>
         <div className="flex flex-col w-2/5 m-auto bg-gray-50 mt-16 shadow-lg">
           <div className="text-5xl py-16 ">Welcome!</div>
-
           <div className="w-3/5 m-auto">
             <FormField label="Email" />
           </div>
-
           <div className="w-3/5 m-auto">
             <FormField label="Password" />
-            <div className="text-xs text-right mb-4">Forgot your password? <button className="text-xs underline">click here</button></div>
+            <div className="text-xs text-right mb-4">
+              Forgot your password?{" "}
+              <button className="text-xs underline">click here</button>
+            </div>
           </div>
-          
-
           <div className="items-center py-2">
             <button className="button button1">Confirm</button>
           </div>
-          <div className="line m-auto"></div>
+          <hr />
           <div className="items-center py-2">
             <button className="button button2">Create new account</button>
           </div>
