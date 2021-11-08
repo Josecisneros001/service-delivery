10c1e5590dcab75701b8271c1ea24af483d0b5f4
josecisneros001 <joseacisnerosm@gmail.com>
2021-11-07 19:44:07 -0600

    remove Navbar unused

--- frontend/src/views/Users.tsx
+++ frontend/src/views/Users.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import UserLogin from '../components/UserLogin/UserLogin';
-import Navbar from '../components/Users/UsersNavbar';
 
 export default class Users extends Component {
 
