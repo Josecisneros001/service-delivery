6be265bbe0b6f3e45ef90438873409d3a423f2bb
Andrea Ramirez <andrearamirezfernandez@gmail.com>
2021-11-10 16:56:32 -0600

    HomePage Service Provider Comienzo + Edit Profile

---
+++ frontend/src/components/ServiceProviders/EditProfile/ServiceProviderEditProfile.tsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import "../../../tailwindcss.css";
+import "../ServiceProviderEditProfile.css";
+{/* import ServiceProviderNavbar from "../ServiceProviders/ServiceProviderNavbar";*/}
+
+class ServiceProviderEditProfile extends Component {
+    render() {
+        return (
+            <>
+            {/* <ServiceProviderNavbar /> */}
+            <h3>Upcoming Reservations</h3>      
+            </>
+        );
+    }
+
+}
+
+export default ServiceProviderEditProfile;
---
+++ frontend/src/components/ServiceProviders/HomePage/ServiceProviderHomePage.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import "../../../tailwindcss.css";
+import "../ServiceProviderHomePage.css";
+import coverProfile from "../../../images/garden.jpeg"
+import profilePic from "../../../images/profilePic.png"
+{/* import ServiceProviderNavbar from "../ServiceProviders/ServiceProviderNavbar";*/}
+
+class ServiceProviderHomePage extends Component{
+    render() {
+        return (
+            <>
+                {/* <ServiceProviderNavbar /> */}
+                <img src="../../../images/profilePic.png" alt="Profile Pic"></img>
+                <div className="coverPhoto">
+                    <div className="tagUsuario">
+                        <p>Bienvenido nombreUsuario !</p>
+                        <p> Rating: #</p>
+                        <button type="button" className="text-xs underline">Edit Profile</button>
+                    </div>
+                </div>
+                <br></br>
+                <h3>Upcoming Reservations</h3>
+                <div className="hover:bg-gray-300 rounded-full h-10 w-10 flex items-center justify-center border-2 border-gray-300 m-2">
+                    <button className="button-crearservicio">+</button>
+                </div>
+
+            </>
+        );
+    }
+
+}
+
+export default ServiceProviderHomePage;
