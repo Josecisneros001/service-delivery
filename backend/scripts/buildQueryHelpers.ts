dd33ccb7899cc0df2d2154188f3a7eaead418da5
Josecisneros001 <joseacisnerosm@gmail.com>
2021-11-10 18:27:12 -0600

    add intersection condition services

--- backend/scripts/buildQueryHelpers.ts
+++ backend/scripts/buildQueryHelpers.ts
@@ -1,30 +1,29 @@
-export const buildWhereClause = (filters: any, params: string[], dbRelations: string[], types: string[]): string => {
-	let paramsValues = [];
+export const buildWhereClause = (filters: any, params: string[], dbRelations: string[], types: string[], conditions: string[] = []): string => {
 	for (let index = 0; index < params.length; index++) {
 		const param = params[index]
 		const dbRelation = dbRelations[index]
 		if ((param in filters)) {
 			if (types[index] == "string") {
-				paramsValues.push(`${dbRelation} LIKE '%${filters[param]}%'`)
+				conditions.push(`${dbRelation} LIKE '%${filters[param]}%'`)
 			} else if (types[index] == "number_min") {
-				paramsValues.push(`${dbRelation} >= '${filters[param]}'`);
+				conditions.push(`${dbRelation} >= '${filters[param]}'`);
 			} else if (types[index] == "number_max") {
-				paramsValues.push(`${dbRelation} <= '${filters[param]}'`);
+				conditions.push(`${dbRelation} <= '${filters[param]}'`);
 			} else if (types[index] == "array") {
 				let tmpOrArray = []
 				for (const filer_value of filters[param]) {
 					tmpOrArray.push(`${dbRelation} = '${filer_value}'`);
 				}
-				paramsValues.push(`(${tmpOrArray.join(' OR ')})`);
+				conditions.push(`(${tmpOrArray.join(' OR ')})`);
 			} else {
-				paramsValues.push(`${dbRelation} = '${filters[param]}'`)
+				conditions.push(`${dbRelation} = '${filters[param]}'`)
 			}
 		}
 	}
-	if (paramsValues.length == 0) {
+	if (conditions.length == 0) {
 		return "";
 	}
-	return `where ${paramsValues.join(" AND ")}`;
+	return `where ${conditions.join(" AND ")}`;
 }
 
 export const buildInsertInto = (params: any, dbTableName: string, fields: string[], timestamp: string): string => {
