9d931b6411965e67359402c8a3fa7cf827900863
clarissavlsqz <clarissa.a.velasquez@gmail.com>
2021-11-10 18:25:38 -0600

    change app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
-import UserSignUp from './components/UserSignUp/UserSignUp';
+// import UserSignUp from './components/UserSignUp/UserSignUp';
+import UserUploadPhoto from './components/UserSignUp/UserUploadPhoto/UserUploadPhoto'
 import './App.css';
 
 function App() {
   return (
     <div className="App h-screen">
-      <UserSignUp />
+      <UserUploadPhoto />
     </div>
   );
 }
