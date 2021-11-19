7d3787117839469a92313c46d61a684bd26590bc
Alex Vargas <alexjesusvargas@gmail.com>
2021-11-19 14:00:07 -0600

    formatted FormField code

--- frontend/src/components/ServiceProviderlogin/FormField/FormField.tsx
+++ frontend/src/components/ServiceProviderlogin/FormField/FormField.tsx
@@ -1,41 +1,44 @@
-import React from 'react';
-import FormFieldI from '../../../Interfaces/UserSignUp/FormField';
-import './FormField.css'
+import React from "react";
+import FormFieldI from "../../../Interfaces/UserSignUp/FormField";
+import "./FormField.css";
 
-class FormField extends React.Component<FormFieldI,{ value: string },{}> {
-    constructor(props: FormFieldI) {
-        super(props);
-        this.state = {value: ''};
+class FormField extends React.Component<FormFieldI, { value: string }, {}> {
+  constructor(props: FormFieldI) {
+    super(props);
+    this.state = { value: "" };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    handleChange(event: React.ChangeEvent<any>) {
-        this.setState({value: event.target.value});
-    }
+  handleChange(event: React.ChangeEvent<any>) {
+    this.setState({ value: event.target.value });
+  }
 
-    handleSubmit(event: React.ChangeEvent<any>) {
-        //alert('A name was submitted: ' + this.state.value);
-        event.preventDefault();
-    }
+  handleSubmit(event: React.ChangeEvent<any>) {
+    //alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
 
-    render() { 
-        return (
-            <form onSubmit={this.handleSubmit} className="flex-1 flex flex-col text-2xl">
-                <div className="w-full text-left">
-                    <label> {this.props.label} </label>
-                </div>
-                <input 
-                    type='text' 
-                    className={`rounded-md w-full h-9 bg-custom-white border-cornflower-blue-500 border-2 p-3 mb-5 text-xl`} 
-                    value={this.state.value} 
-                    onChange={this.handleChange}
-                    placeholder={this.props.placeholder}
-                />
-            </form>
-        );
-    }
+  render() {
+    return (
+      <form
+        onSubmit={this.handleSubmit}
+        className="flex-1 flex flex-col text-2xl"
+      >
+        <div className="w-full text-left">
+          <label> {this.props.label} </label>
+        </div>
+        <input
+          type="text"
+          className={`rounded-md w-full h-9 bg-custom-white border-cornflower-blue-500 border-2 p-3 mb-5 text-xl`}
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder={this.props.placeholder}
+        />
+      </form>
+    );
+  }
 }
 
 export default FormField;
--- frontend/src/components/UserLogin/FormField/FormField.tsx
+++ frontend/src/components/UserLogin/FormField/FormField.tsx
@@ -1,35 +1,35 @@
-import React from 'react';
-import FormFieldI from '../../../Interfaces/UserSignUp/FormField';
-import './FormField.css'
+import React from "react";
+import FormFieldI from "../../../Interfaces/UserSignUp/FormField";
+import "./FormField.css";
 
-class FormField extends React.Component<FormFieldI,{ value: string },{}> {
-    constructor(props: FormFieldI) {
-        super(props);
-        this.state = {value: ''};
-        this.handleChange = this.handleChange.bind(this);
-    }
+class FormField extends React.Component<FormFieldI, { value: string }, {}> {
+  constructor(props: FormFieldI) {
+    super(props);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-    handleChange(event: React.ChangeEvent<any>) {
-        this.setState({value: event.target.value});
-        this.props.onChange(event.target.value);
-    }
+  handleChange(event: React.ChangeEvent<any>) {
+    this.setState({ value: event.target.value });
+    this.props.onChange(event.target.value);
+  }
 
-    render() { 
-        return (
-            <>
-                <div className="w-full text-left">
-                    <label> {this.props.label} </label>
-                </div>
-                <input 
-                    type='text' 
-                    className={`rounded-md w-full h-9 bg-custom-white border-cornflower-blue-500 border-2 p-3 mb-5 text-xl`} 
-                    value={this.state.value} 
-                    onChange={this.handleChange}
-                    placeholder={this.props.placeholder}
-                />
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="w-full text-left">
+          <label> {this.props.label} </label>
+        </div>
+        <input
+          type="text"
+          className={`rounded-md w-full h-9 bg-custom-white border-cornflower-blue-500 border-2 p-3 mb-5 text-xl`}
+          value={this.state.value}
+          onChange={this.handleChange}
+          placeholder={this.props.placeholder}
+        />
+      </>
+    );
+  }
 }
 
 export default FormField;
