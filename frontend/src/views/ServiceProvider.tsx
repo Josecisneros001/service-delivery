05d75566ea2a9377db0d9beef6030b272e47a7bf
Alex Vargas <alexjesusvargas@gmail.com>
2021-11-20 14:16:38 -0600

    added edit service component

--- frontend/src/views/ServiceProvider.tsx
+++ frontend/src/views/ServiceProvider.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from '../components/ServiceProviders/ServiceProviderNavbar';
 import CreateNewService from '../components/ServiceProviders/CreateNewService';
 import Test from '../components/ServiceProviders/Cards/Cards';
+import EditService from '../components/ServiceProviders/EditService';
 
 export default class ServiceProvider extends Component {
 
@@ -14,6 +15,7 @@ export default class ServiceProvider extends Component {
         {/* TODO: Handle all ServiceProvider Components */}
         <Routes>
           <Route path="/create-service" element={<CreateNewService/>} />
+          <Route path="/edit-service" element={<EditService/>} />
           <Route path="/card" element={<Test/>} />
         </Routes>
       </div>
