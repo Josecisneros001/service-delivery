6ed9d19f34388fe3b8dd1457c717261a9d75d3e0
Alex Vargas <alexjesusvargas@gmail.com>
2021-11-20 15:17:48 -0600

    added cards to home page

--- frontend/src/components/ServiceProviders/HomePage/ServiceProviderHomePage.tsx
+++ frontend/src/components/ServiceProviders/HomePage/ServiceProviderHomePage.tsx
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../../../tailwindcss.css";
 import "./ServiceProviderHomePage.css";
-import coverProfile from "../../../images/garden.png"
-import ServiceProviderNavbar from "../ServiceProviderNavbar";
+import Reservations from "../Reservations/Reservations";
 
-class ServiceProviderHomePage extends Component{
-    render() {
-        return (
-            <>
-                <div className="coverPhoto">
-                    <div className="tagUsuario">
-                        <p>Bienvenido nombreUsuario !</p>
-                        <p> Rating: #</p>
-                        <button type="button" className="text-xs underline">Edit Profile</button>
-                    </div>
-                </div>
-                <br></br>
-                <h1>Upcoming Reservations</h1>
-                <div className="hover:bg-gray-300 rounded-full h-10 w-10 flex items-center justify-center border-2 border-gray-300 m-2">
-                    <button className="button-crearservicio">+</button>
-                </div>
+class ServiceProviderHomePage extends Component {
+  render() {
+    return (
+      <>
+        <div className="coverPhoto">
+          <div className="tagUsuario">
+            <p>Bienvenido nombreUsuario !</p>
+            <p> Rating: #</p>
+            <button type="button" className="text-xs underline">
+              Edit Profile
+            </button>
+          </div>
+        </div>
+        <br></br>
+        <h1 className="text-3xl">Upcoming Reservations</h1>
+        <div className="grid justify-items-end">
+          <div className="hover:bg-gray-300 rounded-full h-10 w-40 flex items-center justify-center border-2 border-gray-300 m-2">
+            <button>Add service</button>
+          </div>
+        </div>
 
-            </>
-        );
-    }
-
+        <Reservations />
+      </>
+    );
+  }
 }
 
 export default ServiceProviderHomePage;
--- frontend/src/views/ServiceProvider.tsx
+++ frontend/src/views/ServiceProvider.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from '../components/ServiceProviders/ServiceProviderNavbar';
 import CreateNewService from '../components/ServiceProviders/CreateNewService';
 import Reservations from '../components/ServiceProviders/Reservations/Reservations';
+import ServiceProviderHomePage from '../components/ServiceProviders/HomePage/ServiceProviderHomePage';
 
 export default class ServiceProvider extends Component {
 
@@ -15,6 +16,7 @@ export default class ServiceProvider extends Component {
         <Routes>
           <Route path="/create-service" element={<CreateNewService/>} />
           <Route path="/reservations" element={<Reservations/>} />
+          <Route path="/homepage" element={<ServiceProviderHomePage/>} />
         </Routes>
       </div>
     );
