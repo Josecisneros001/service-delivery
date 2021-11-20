c2cd7d0aa33118a6f2a0ac532b9d975ec63ce8dd
Alex Vargas <alexjesusvargas@gmail.com>
2021-11-20 15:28:22 -0600

    formatted code

--- frontend/src/components/ServiceProviders/HomePage/ServiceProviderHomePage.tsx
+++ frontend/src/components/ServiceProviders/HomePage/ServiceProviderHomePage.tsx
@@ -3,31 +3,29 @@ import "../../../tailwindcss.css";
 import "./ServiceProviderHomePage.css";
 import Reservations from "../Reservations/Reservations";
 
-class ServiceProviderHomePage extends Component {
-  render() {
-    return (
-      <>
-        <div className="coverPhoto">
-          <div className="tagUsuario">
-            <p>Bienvenido nombreUsuario !</p>
-            <p> Rating: #</p>
-            <button type="button" className="text-xs underline">
-              Edit Profile
-            </button>
-          </div>
+const ServiceProviderHomePage = () => {
+  return (
+    <>
+      <div className="coverPhoto">
+        <div className="tagUsuario">
+          <p>Bienvenido nombreUsuario !</p>
+          <p> Rating: #</p>
+          <button type="button" className="text-xs underline">
+            Edit Profile
+          </button>
         </div>
-        <br></br>
-        <h1 className="text-3xl">Upcoming Reservations</h1>
-        <div className="grid justify-items-end">
-          <div className="hover:bg-gray-300 rounded-full h-10 w-40 flex items-center justify-center border-2 border-gray-300 m-2">
-            <button>Add service</button>
-          </div>
+      </div>
+      <br></br>
+      <h1 className="text-3xl">Upcoming Reservations</h1>
+      <div className="grid justify-items-end">
+        <div className="hover:bg-gray-300 rounded-full h-10 w-40 flex items-center justify-center border-2 border-gray-300 m-2">
+          <button>Add service</button>
         </div>
+      </div>
 
-        <Reservations />
-      </>
-    );
-  }
-}
+      <Reservations />
+    </>
+  );
+};
 
 export default ServiceProviderHomePage;
