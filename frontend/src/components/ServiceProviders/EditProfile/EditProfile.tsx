0c2ce553c300d25a8d9a16bfc2a103f422a6f8e9
Josecisneros001 <joseacisnerosm@gmail.com>
2021-11-26 03:15:42 -0600

    last details - edit profile

--- frontend/src/components/ServiceProviders/EditProfile/EditProfile.tsx
+++ frontend/src/components/ServiceProviders/EditProfile/EditProfile.tsx
@@ -1,24 +1,25 @@
 import { Component } from "react";
-import "./EditProfile.css";
-import UsersNavbarLogin from "../../Users/UsersNavbarLogin";
-import ServiceProviderNavbarLogin from "../../ServiceProviders/ServiceProviderNavbarLogin";
 import FormField from "../../General/FormField/FormField";
 import { Users as UsersModel } from "../../../interfaces/models/Users";
 import { Users } from "../../../scripts/APIs/Users";
 import { getCurrentUser } from "../../../scripts/APIs";
-import { Navigate } from "react-router-dom";
 import Snackbar from '../../General/Snackbar';
+import ServiceProviderNavbar from "../ServiceProviderNavbar";
+import UsersNavbar from "../../Users/UsersNavbar";
 
 export interface EditProfileState {
-	userInfo: UsersModel | null,
+	userInfo: UsersModel | undefined,
 	firstName: string;
 	lastName: string;
 	password: string;
 	email: string;
+    recovEmail: string;
 	phone: string;
+    altPhone: string;
 	snackBarMsg: string;
 	showAlert: boolean;
-	[key: string]: UsersModel | string | boolean | null;
+    isLoading: boolean;
+	[key: string]: UsersModel | string | boolean | undefined;
 };
 
 class EditProfile extends Component<
@@ -29,15 +30,17 @@ class EditProfile extends Component<
     constructor(props: { is_service_provider: boolean }) {
         super(props);
         this.state = {
-			userInfo: null,
+			userInfo: undefined,
             firstName: "",
 			lastName: "",
             password: "",
             email: "",
+            recovEmail: "",
             phone: "",
+            altPhone: "",
             snackBarMsg: "",
           	showAlert: false,
-            isDone: false
+            isLoading: false
         };
     
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -67,7 +70,6 @@ class EditProfile extends Component<
         this.setState({firstName: firstNameValue})
     }
 
-
     handleLastName = (lastNameValue: string) => {
         this.setState({lastName: lastNameValue})
     }
@@ -76,15 +78,22 @@ class EditProfile extends Component<
         this.setState({phone: phoneValue})
     }
 
+    handleAltPhone = (phoneValue: string) => {
+        this.setState({altPhone: phoneValue})
+    }
 
     handleEmail = (emailValue: string) => {
         this.setState({ email: emailValue });
     };
 
+    handleRecovEmail = (emailValue: string) => {
+        this.setState({recovEmail: emailValue });
+    };
+
 	formValidations = () => {
         //checar que hay info en todas
-        const fields = ["firstName", "lastName", "email", "phone"];
-        const fieldsName = ["First Name", "Last Name", "Email","Phone Number"];
+        const fields = ["firstName", "lastName", "email", "recovEmail", "phone", "altPhone"];
+        const fieldsName = ["First Name", "Last Name", "Email", "Recovery Email", "Phone Number", "Alt. Phone"];
         for(const index in fields) {
             const field = fields[index];
             const fieldName = fieldsName[index];
@@ -100,22 +109,27 @@ class EditProfile extends Component<
             return false;
         }
 
+        if (!re.test(this.state.recovEmail)) {
+            this.setState({snackBarMsg: "Invalid Recovery Email", showAlert: true});
+            return false;
+        }
+
         return true;
-      };
+    };
 
 	async handleSubmit(event: React.ChangeEvent<any>) {
         event.preventDefault();
         if (!this.formValidations()) {
           return;
         }
-
+        this.setState({isLoading: true});
         const params = {
             first_name: this.state.firstName,
             last_name: this.state.lastName,
-            password: this.state.password,
             email: this.state.email,
+            recovery_email: this.state.recovEmail,
             phone_number: this.state.phone,
-            is_service_provider: this.props.is_service_provider ? 1 : 0
+            alt_phone_number: this.state.altPhone,
         } as UsersModel;
 
         const response = await Users.update(
@@ -123,82 +137,95 @@ class EditProfile extends Component<
             getCurrentUser(this.props.is_service_provider),
         );
         if (response.status !== 200) {
-          this.setState({
-            snackBarMsg: "Could not update the information",
-            showAlert: true,
-          });
-          return;
+            this.setState({
+                snackBarMsg: " Server Error - Try Again Later",
+                showAlert: true,
+            });
+            this.setState({isLoading: false});
+            return;
         }
-
-        this.setState({isDone: true});
+        
+        this.setState({
+            snackBarMsg: "Saved Successfully",
+            showAlert: true,
+        });
+        this.setState({isLoading: false});
       }
 
     render() {
-        if (this.state.isDone) {
-            if (this.props.is_service_provider) {
-                return <Navigate to="/service-providers/homepage" />
-            } else {
-                return <Navigate to="/users/" />
-            }
-        }
-
         return (
             <>
-			<Snackbar
-				{...this.configSnackbar}
-				show={this.state.showAlert}
-				hideEvent={this.hideSnackbar}
-				message={this.state.snackBarMsg}
-			/>
-            {this.props.is_service_provider ? (
-                <ServiceProviderNavbarLogin />
-            ) : (
-                <UsersNavbarLogin />
-            )}
-
-            <div className="coverPhoto">
-                <div className="flex flex-col w- 3/5 md:w-2/5 m-auto bg-blue-100 mt-16 shadow-lg text-center rounded-3xl py-9">
+                <Snackbar
+                    {...this.configSnackbar}
+                    show={this.state.showAlert}
+                    hideEvent={this.hideSnackbar}
+                    message={this.state.snackBarMsg}
+                />
+                {this.props.is_service_provider ? (
+                    <ServiceProviderNavbar user={this.state.userInfo} />
+                ) : (
+                    <UsersNavbar user={this.state.userInfo} />
+                )}
+                <div className="flex flex-col w-full md:w-3/5 lg:w-2/5 m-auto bg-blue-100 mt-16 shadow-lg text-center rounded-3xl py-9"
+                    style={{opacity: this.state.isLoading? '0.5' : '1'}}
+                >
+                    <div className="text-3xl w-full pb-3">Update User Info</div>
                     <form
-                    onSubmit={this.handleSubmit}
-                    className="flex-1 flex flex-col text-2xl"
+                        onSubmit={this.handleSubmit}
+                        className="flex-1 flex flex-col text-2xl"
                     >
                         <div className="w-3/5 m-auto">
-                        <FormField
-                            orientation="col"
-                            label="First Name"
-                            onChange={this.handleFirstName}
-                            initialValue={this.state.userInfo?.first_name}
-                        />
+                            <FormField
+                                orientation="col"
+                                label="First Name"
+                                onChange={this.handleFirstName}
+                                initialValue={this.state.userInfo?.first_name}
+                            />
                         </div>
 
                         <div className="w-3/5 m-auto">
+                            <FormField
+                                orientation="col"
+                                label="Last Name"
+                                onChange={this.handleLastName}
+                                initialValue={this.state.userInfo?.last_name}
+                            />
                         </div>
 
                         <div className="w-3/5 m-auto">
-                        <FormField
-                            orientation="col"
-                            label="Last Name"
-                            onChange={this.handleLastName}
-                            initialValue={this.state.userInfo?.last_name}
-                        />
+                            <FormField
+                                orientation="col"
+                                label="Email"
+                                onChange={this.handleEmail}
+                                initialValue={this.state.userInfo?.email}
+                            />
                         </div>
 
                         <div className="w-3/5 m-auto">
-                        <FormField
-                            orientation="col"
-                            label="Email"
-                            onChange={this.handleEmail}
-                            initialValue={this.state.userInfo?.email}
-                        />
+                            <FormField
+                                orientation="col"
+                                label="Recovery Email"
+                                onChange={this.handleRecovEmail}
+                                initialValue={this.state.userInfo?.recovery_email}
+                            />
                         </div>
 
                         <div className="w-3/5 m-auto">
-                        <FormField
-                            orientation="col"
-                            label="Phone"
-                            onChange={this.handlePhone}
-                            initialValue={this.state.userInfo?.phone_number}
-                        />
+                            <FormField
+                                orientation="col"
+                                label="Phone"
+                                onChange={this.handlePhone}
+                                initialValue={this.state.userInfo?.phone_number}
+                            />
+                        </div>
+
+                        <div className="w-3/5 m-auto">
+                            <FormField
+                                orientation="col"
+                                label="Phone"
+                                onChange={this.handleAltPhone}
+                                initialValue={this.state.userInfo?.alt_phone_number}
+                            />
                         </div>
 
                         <div className="items-center py-2">
@@ -208,9 +235,6 @@ class EditProfile extends Component<
                         </div>
                     </form>
                 </div>
-
-                <br></br>
-            </div>
             </>
         );
     }
