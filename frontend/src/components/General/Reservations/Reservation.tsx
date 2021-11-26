ecf4743e9986119e8a612437c37faff1dec97c16
Josecisneros001 <joseacisnerosm@gmail.com>
2021-11-26 03:22:56 -0600

    add user info to reservations

--- frontend/src/components/General/Reservations/Reservation.tsx
+++ frontend/src/components/General/Reservations/Reservation.tsx
@@ -30,16 +30,23 @@ const Reservation = (props: {
           </div>
         </div>
         <div className="flex flex-col">
-          <span>{`${props.client.first_name} ${props.client.last_name}`}</span>
-          <NavLink
-            to={`/service-providers/chats?user_id=${props.client.id}`}
-          >
-            <img
-              className="h-14 w-14 rounded-full object-cover ml-auto mr-2"
-              src={getFileUrl(props.client.profile_picture)}
-              alt="a"
-            />
-          </NavLink>
+          <div className="flex flex-row w-full">
+            <div className="flex-1 flex flex-col text-sm text-right mr-2 mt-auto">
+              <span>{`${props.client.first_name} ${props.client.last_name}`}</span>
+              <span>{`${props.client.email}`}</span>
+              <span>{`${props.client.phone_number}`}</span>
+            </div>
+            <NavLink
+              to={`/service-providers/chats?user_id=${props.client.id}`}
+              className="flex-initial"
+            >
+              <img
+                className="h-14 w-14 rounded-full object-cover mr-2"
+                src={getFileUrl(props.client.profile_picture)}
+                alt="a"
+              />
+            </NavLink>
+          </div>
         </div>
       </div>
       <div className="w-full h-48" style={{display: showMap? 'block': 'none'}}>
