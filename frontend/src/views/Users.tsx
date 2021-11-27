cdc8c572a85f13d5f418b7290baaa41c29de02be
Josecisneros001 <joseacisnerosm@gmail.com>
2021-11-27 01:33:29 -0600

    hotfix paths

--- frontend/src/views/Users.tsx
+++ frontend/src/views/Users.tsx
@@ -4,6 +4,7 @@ import Login from '../components/Login/Login';
 import SignUp from '../components/SignUp/SignUp';
 import PrivateRoute from '../scripts/PrivateRoute';
 import UploadFilesForm from '../components/SignUp/UploadFilesForm/UploadFilesForm';
+import EditProfile from '../components/ServiceProviders/EditProfile/EditProfile';
 import Chats from '../components/Chats/Chats';
 import RequestPage from '../components/Users/RequestPage';
 import UserHomePage from '../components/Users/UserHomePage/UserHomePage';
@@ -18,10 +19,14 @@ export default class Users extends Component {
           <Route path="/login" element={<Login is_service_provider={false}/>}/>
           <Route path="/signup" element={<SignUp is_service_provider={false}/>}/>
           <Route
+            path="/edit-profile"
+            element={<PrivateRoute is_service_provider={false} redirectTo="login"><EditProfile is_service_provider={false}/></PrivateRoute>}
+          />
+          <Route
             path="/upload-files"
             element={<PrivateRoute is_service_provider={false} redirectTo="login"><UploadFilesForm is_service_provider={false}/></PrivateRoute>}
           />
-         <Route
+          <Route
             path="/chats"
             element={<PrivateRoute is_service_provider={false} redirectTo="login"><Chats is_service_provider={false}/></PrivateRoute>}
           />
@@ -37,7 +42,6 @@ export default class Users extends Component {
             path="/"
             element={<PrivateRoute is_service_provider={false} redirectTo="login"><UserHomePage/></PrivateRoute>}
           />
-          {/* TODO: Change * to Home View */}
           <Route
             path="*"
             element={<PrivateRoute is_service_provider={false} redirectTo="login"><UserHomePage /></PrivateRoute>}
